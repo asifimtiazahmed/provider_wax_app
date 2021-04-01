695953f6fcde2de5014ecaa3f229542dfdd4d5af
asifimtiazahmed <a.imtiaz@gmail.com>
2021-03-31 23:43:21 -0400

    update for name

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: ,
+      create: (),
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Wax App',
