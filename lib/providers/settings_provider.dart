1141e9d173078e35a1b1ed780e5ed74c9c298317
asifimtiazahmed <a.imtiaz@gmail.com>
2021-04-01 22:55:01 -0400

    Setting_provider setup

--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -1,3 +1,40 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class SettingsProvider with ChangeNotifier
+class SettingsProvider with ChangeNotifier{
+  String _units;
+  List<String> _waxLines;
+
+  SettingsProvider(){
+    _units = "Imperial";
+    _waxLines = ['Swix','Toko'];
+  }
+
+  //Getters
+  String get units => _units;
+  List<String> get waxLines => _waxLines;
+
+  //Setters
+  void setUnits(String units){
+    _units = units;
+    notifyListeners();
+  }
+
+  void _setWaxLines(List<String> waxLines){
+    _waxLines = waxLines;
+    notifyListeners();
+  }
+
+  void addWaxLine(String waxLine){
+    if(_waxLines.contains(waxLine) == false){
+      _waxLines.add(waxLine);
+      notifyListeners();
+    }
+
+    void removeWaxLine(String waxLine){
+      if(_waxLines.contains(waxLines)== true){
+        _waxLines.remove(waxLines);
+        notifyListeners();
+      }
+    }
+  }
+}
