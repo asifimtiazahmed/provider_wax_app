4d12d6e0f3b5cbf17b93527f2ae523ff510a8fa3
asifimtiazahmed <a.imtiaz@gmail.com>
2021-04-01 21:59:56 -0400

    begining to setup change notifier

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
-      create: (),
+      create: (BuildContext context) => ,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Wax App',
